4189d26f7c6e68cc7449e087aa2f803efd17e6d1
Simon Ha <simon9ha@gmail.com>
2022-02-24 19:42:50 -0500

    initial commit

---
+++ src/main/java/ftpclient/Main.java
@@ -0,0 +1,11 @@
+package ftpclient;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		if (args.length != 4) {
+			System.out.println("Usage: java -jar ftpclient-0.0.1-SNAPSHOT.jar server port user password");
+		}
+	}
+
+}
